59f302407393838894f7481c157a67a6e2d47feb
Eugenio <lugezz@gmail.com>
2023-08-24 09:42:29 -0300

    Excecise factorial

---
+++ recursion/factorial_excercise 1.py
@@ -0,0 +1,24 @@
+"""
+What if somebody wants to check the parameters in a recursive function?
+For example the factorial function.
+Okay, please write a recursive version of factorial, which checks the
+parameters. Maybe you did it in a perfect way, but maybe not.
+Can you improve your version? Get rid of unnecessary tests?
+"""
+
+
+def factorial(n):
+    """ Calculates the factorial of n,
+        n should be an integer and n <= 0 """
+    def inner_factorial(n):
+        if n == 0:
+            return 1
+        else:
+            return n * inner_factorial(n-1)
+    if type(n) == int and n >= 0:
+        return inner_factorial(n)
+    else:
+        raise TypeError("n should be a positve int or 0")
+
+
+print(factorial(10))
