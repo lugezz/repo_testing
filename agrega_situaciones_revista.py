4b48c66c464a035d0b50eb8474567f6487c019c9
Eugenio Vazquez <lugezz@gmail.com>
2024-04-28 10:19:50 -0300

    Adding some recursion

--- agrega_situaciones_revista.py
+++ agrega_situaciones_revista.py
@@ -34,23 +34,25 @@ situaciones_revista_3 = [
     },
 ]
 
-situaciones_revista_1 = [
-    {
-        'inicio': 1,
-        'fin': 3,
-        'situacion': 10,
-    },
-    {
-        'inicio': 5,
-        'fin': 7,
-        'situacion': 2,
-    },
-    {
-        'inicio': 19,
-        'fin': 30,
-        'situacion': 3,
-    },
+situaciones_revista_4 = [
+    {'inicio': 10, 'fin': 12, 'situacion': 19},
+    {'inicio': 28, 'fin': 29, 'situacion': 12},
 ]
+
+
+def fill_gaps(resp, index=0):
+    if index == len(resp) - 1:
+        return resp
+
+    if resp[index]['fin'] + 1 != resp[index + 1]['inicio']:
+        resp.insert(index + 1, {
+            'inicio': resp[index]['fin'] + 1,
+            'fin': resp[index + 1]['inicio'] - 1,
+            'situacion': 1,
+        })
+        return fill_gaps(resp, index + 1)
+    else:
+        return fill_gaps(resp, index + 1)
 
 
 def agrega_situaciones_revista(situaciones_revista: list) -> list:
@@ -65,13 +67,8 @@ def agrega_situaciones_revista(situaciones_revista: list) -> list:
             'situacion': 1,
         })
 
-    for i in range(len(resp) - 1):
-        if resp[i]['fin'] + 1 != resp[i + 1]['inicio']:
-            resp.append({
-                'inicio': resp[i]['fin'] + 1,
-                'fin': resp[i + 1]['inicio'] - 1,
-                'situacion': 1,
-            })
+    # Fill the gaps between the situations
+    resp = fill_gaps(resp)
 
     # If the last situation doesn't end at 30, add a new situation
     if resp[-1]['fin'] != 30:
@@ -87,6 +84,7 @@ def agrega_situaciones_revista(situaciones_revista: list) -> list:
 fixed_sr1 = agrega_situaciones_revista(situaciones_revista_1)
 fixed_sr2 = agrega_situaciones_revista(situaciones_revista_2)
 fixed_sr3 = agrega_situaciones_revista(situaciones_revista_3)
+fixed_sr4 = agrega_situaciones_revista(situaciones_revista_4)
 
 
 print("-" * 100)
@@ -102,6 +100,11 @@ print("-" * 100)
 print("-" * 100)
 print("Situaciones Revista 3")
 pprint(fixed_sr3)
+print("-" * 100)
+
+print("-" * 100)
+print("Situaciones Revista 4")
+pprint(fixed_sr4)
 print("-" * 100)
 
 
@@ -120,7 +123,7 @@ def depura_situaciones_revista(situaciones_revista: list) -> list:
     return resp
 
 
-situaciones_revista_4 = [
+situaciones_revista_50 = [
     {
         'inicio': 1,
         'fin': 3,
@@ -139,9 +142,10 @@ situaciones_revista_4 = [
 ]
 
 
-fixed_sr4 = depura_situaciones_revista(situaciones_revista_4)
+fixed_sr50 = depura_situaciones_revista(situaciones_revista_50)
 
+print("=" * 100)
 print("-" * 100)
-print("Situaciones Revista 4")
-pprint(fixed_sr4)
+print("Situaciones Revista 50")
+pprint(fixed_sr50)
 print("-" * 100)
